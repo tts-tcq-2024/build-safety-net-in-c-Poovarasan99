d06b765656825e5f2962e8a6eb6c476ece7c7a85
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2024-07-04 14:16:50 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,6 @@
 TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[5];
   generateSoundex("AX", soundex);
-  printf("%c%c%c%%c\n",soundex[0],soundex[1],soundex[2],soundex[3]);
+  printf("%c%c%c%c%c\n",soundex[0],soundex[1],soundex[2],soundex[3],soundex[4]);
   ASSERT_EQ(soundex,"A200");
 }
