f9e932af9db55a795a15ac890dadfbccb99dca66
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2024-07-04 11:39:56 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,14 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigitsCase1) {
  char soundex[5];
- EXPECT_CALL(SoudexTestsuite, getSoundexCode('X').Times(1));
  generateSoundex("AX", soundex);
  ASSERT_EQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigitsCase2) {
+ char soundex[5];
+ generateSoundex("AEIOU", soundex);
+ ASSERT_EQ(soundex,"A000");
 }
