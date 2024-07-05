6beb85bd70b40e768372ede002dda5c3504dd5f7
Poovarasan99 <144001340+Poovarasan99@users.noreply.github.com>
2024-07-05 20:41:50 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -25,5 +25,12 @@ INSTANTIATE_TEST_SUITE_P(TestParameters, SoundexTestsuite,
         std::make_tuple("AX", "A200"),
         std::make_tuple("", "000"),
         std::make_tuple("A", "A000")
+        std::make_tuple("Robert", "R163")
+        std::make_tuple("Ropert", "R163")
+        std::make_tuple("Rubin", "R150")
+        std::make_tuple("Ashcraft", "A261")
+        // std::make_tuple("tymczak", "T522")
+        // std::make_tuple("Pfister", "P236")
+        // std::make_tuple("honeyman", "H555")
     )
 );
